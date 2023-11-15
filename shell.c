fe759fae4a56a0959da149db91d6286523012f66
hassan629 <hsnh83099.com>
2023-11-15 22:19:49 +0200

    done

--- shell.c
+++ shell.c
@@ -14,9 +14,7 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		if (interactive)
-		{
 			printf("$ ");
-		}
 
 		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
 		{
@@ -29,17 +27,11 @@ int main(int argc, char *argv[])
 		command[strcspn(command, "\n")] = '\0';
 
 		if (strcmp(command, "exit") == 0)
-		{
 			comm_exit("0");
-		}
 		else if (strcmp(command, "env") == 0)
-		{
 			env();
-		}
 		else if (strncmp(command, "cd ", 3) == 0)
-		{
 			cd(command + 3);
-		}
 		else
 		{
 			pid_t pid = fork();
