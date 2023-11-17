09b441ef256c61b79ea99446a963cb4a8f606b89
hassan629 <hsnh83099.com>
2023-11-17 19:45:14 +0200

    done

---
+++ exec_com.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * exec_com - function to execute command
+ * @args: arguments list
+ * Return: exit status, -1 otherwise
+ */
+
+int exec_com(char *const args[])
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		execvp(args[0], args);
+		perror("execvp");
+		_exit(1);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		return (status);
+	}
+}
+
