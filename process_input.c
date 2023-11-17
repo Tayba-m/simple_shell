4fbd3c4e0cb18eb912f0557268e3f11188ca401a
hassan629 <hsnh83099.com>
2023-11-17 19:47:17 +0200

    done

---
+++ process_input.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+
+/**
+ * part_input - functon part input
+ * @input_str : arg
+ * @arg_count : arg
+ * @exit_status : arg
+ *
+ * Return: args
+ */
+char **part_input(char *input_str, int *arg_count, int *exit_status)
+{
+	char *token;
+	char **args = NULL;
+	*arg_count = 0;
+
+	token = strtok(input_str, " ");
+
+	while (token != NULL)
+	{
+		if (strcmp(token, "$?") == 0)
+		{
+			char exit_status_str[4];
+
+			snprintf(exit_status_str, sizeof(exit_status_str), "%d", *exit_status);
+			args = realloc(args, (*arg_count + 1) * sizeof(char *));
+			args[*arg_count] = strdup(exit_status_str);
+		}
+		else
+		{
+			if (strcmp(token, "$$") == 0)
+			{
+				char pid_str[10];
+
+				snprintf(pid_str, sizeof(pid_str), "%d", getpid());
+				args = realloc(args, (*arg_count + 1) * sizeof(char *));
+				args[*arg_count] = strdup(pid_str);
+			}
+			else
+			{
+				args = realloc(args, (*arg_count + 1) * sizeof(char *));
+				args[*arg_count] = strdup(token);
+			}
+		}
+		(*arg_count)++;
+		token = strtok(NULL, " ");
+	}
+	return (args);
+}
+
+/**
+ * process_input - function process input
+ * @input_str : arg
+ *
+ * Return: nothing
+ */
+void process_input(char *input_str)
+{
+	char **args = NULL;
+	int arg_count = 0;
+	int exit_status = -1;
+	int i;
+
+	args = part_input(input_str, &arg_count, &exit_status);
+
+	if (arg_count > 0)
+	{
+		args = realloc(args, (arg_count + 1) * sizeof(char *));
+		args[arg_count] = NULL;
+		exit_status = exec_com(args);
+		i = 0;
+
+		while (i <= arg_count)
+		{
+			free(args[i]);
+			i++;
+		}
+		free(args);
+		args = NULL;
+		arg_count = 0;
+	}
+}
